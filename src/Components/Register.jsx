a728cedd3a9795373e4ffac04be5a23168c7f4d1
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-04-17 19:19:20 +0600

    register added and validation show

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,18 +1,47 @@
-import React from 'react';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import React from "react";
+import app from "../firebase/firebase.init";
 
 const Register = () => {
-    return (
-        <div>
-            <h3>Register</h3>
-            <form>
-                <input type="email" name="username" id="username" placeholder='Your Email' />
-                <br />
-                <input type="password" name="password" id="password" placeholder='Your Password' />
-                <br />
-                <input type="submit" value="Register" />
-            </form>
-        </div>
-    );
+    const [error , setError] = React.useState("");
+    const [success , setSuccess] = React.useState("");
+
+  const auth = getAuth(app);
+  
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSuccess("");
+    const password = e.target.password.value;
+    const email = e.target.email.value;
+    createUserWithEmailAndPassword(auth, email, password).then((result) => {
+        setError("");
+        e.target.reset();
+        setSuccess("Registration Successful");
+    }).catch((error) => {
+        setError(error.message);
+    });
+  };
+
+  return (
+    <div>
+      <h3>Register</h3>
+      <form onSubmit={handleSubmit}>
+        <input required type="email" name="email" id="email" placeholder="Your Email" />
+        <br />
+        <input required
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Your Password"
+        />
+        <br />
+        <p>{error}</p>
+        <p>{success}</p>
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  );
 };
 
 export default Register;
